07b5d8431cf38c89342675f784ce65751d499902
夜枫 <zdjzxyy@163.com>
2016-06-16 11:55:14 +0800

    Revert "add-qing Qia hui"
    This reverts commit 263ff841b7f705b2111afd1a3028480aab636410.

--- hhs/js/home.js
+++ hhs/js/home.js
@@ -2861,16 +2861,45 @@ if (typeof Object.create !== "function") {
 }(jQuery, window, document));
 
 
+
+
+
+
+
+
+
+
+
+
 **/
 
+
+
+
+
 $(function() {
+	//  $(".flexslider").flexslider();
 
 	$('.big-slider ul').owlCarousel({
 		items: 1,
 		loop: true,
 		autoplay: 3000
+
+
 	});
 
+
+	//    $("#srcoll-x-content").flexslider({
+	// 	animation: "slide",
+	//        animationLoop: true,
+	//        itemWidth: 320,
+	//        itemMargin: 0,
+	// 	minItems: 2,
+	//        maxItems: 4,
+	//         slideDirection: "vertical"
+	//         // randomize: true,
+	//       // pausePlay: true
+	// });
 	if ($("#srcoll-x-content ul").length > 0) {
 		$("#srcoll-x-content ul").owlCarousel({
 			items: 4,
@@ -2882,6 +2911,7 @@ $(function() {
 			nav: false
 		});
 	}
+
 
 	$('.owl-carousel').owlCarousel({
 		items: 1,
@@ -2896,47 +2926,4 @@ $(function() {
 			}
 		}
 	});
-
-
-});
-
-
-
-
-$(document).ready(function(){
-	function unfoldMenu(pn, cn){
-		var p = $('.order-title'), c = $('div.menu-c'), cc = $('div.menu-c-current');
-		if(c.index(cn) != c.index(cc)){
-			p.removeClass('menu-p-current');
-			cc.hide(100, function(){
-				$(this).removeAttr('style').removeClass('menu-c-current');
-			})
-			pn.addClass('menu-p-current');
-			cn.show(100, function(){
-				$(this).removeAttr('style').addClass('menu-c-current');
-			});
-		}
-	}
-	
-	function menuHandle(){
-		$('.order-title').hover(function(){
-			var pn = $(this), cn = pn.next();
-			unfoldMenu(pn, cn);
-		});
-	}
-	
-	//设置默认下当前展开
-	function menuCurrent(){
-		var idx = $('input.menu-code-index').val(), m, pn, cn, p = $('.order-title'), c = $('div.menu-c'), cc = $('div.menu-c-current');
-		if(/c(\d)+/.test(idx)){ //判断c（十进制）条件
-			m = $('a[data-service-index="' + idx + '"]').addClass('current');
-			cn = m.parents('div.menu-c');
-			pn = cn.prev();
-			unfoldMenu(pn, cn);
-		}
-	}
-	
-	menuCurrent();
-	menuHandle();
-	
 });
